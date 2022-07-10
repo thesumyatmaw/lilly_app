308725ade21bdf7c2b3372fd0830b1e844ebd49d
thesumyatmaw <thesumyatmaw@gmail.com>
2022-07-10 17:53:39 +0200

    domain url

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:8080/',
+    // baseUrl: 'http://localhost:8080/',
+    baseUrl : 'https://littleui-app-git-master-thanthtetzin.vercel.app/',
   },
 })
